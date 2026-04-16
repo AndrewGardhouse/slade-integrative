4762537eed7e8e9448a163ecfde3fdd6e942b46f
AndrewGardhouse <andrewgardhouse@gmail.com>
2026-04-16 14:35:24 -0400

    Add sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,5 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sladeintegrative.com',
-  integrations: [tailwind(), image()]
+  integrations: [tailwind(), image({ serviceEntryPoint: '@astrojs/image/sharp' })]
 });
